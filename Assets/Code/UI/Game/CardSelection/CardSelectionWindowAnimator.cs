79a5b9b22ee836878c0fb4e9f8ba2122cd55f7cf
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-09-12 13:43:20 +0300

    edit: animation card selection window

--- Assets/Code/UI/Game/CardSelection/CardSelectionWindowAnimator.cs
+++ Assets/Code/UI/Game/CardSelection/CardSelectionWindowAnimator.cs
@@ -9,13 +9,22 @@ namespace Code.UI.Game.CardSelection
     public class CardSelectionWindowAnimator : MonoBehaviour
     {
         [SerializeField] private List<CanvasGroup> _canvasGroups;
+        [SerializeField] private RectTransform _rollCardsButton;
+        [SerializeField] private RectTransform _selectButton;
         [SerializeField] private float _duration = 0.2f;
         [SerializeField] private Ease _ease = Ease.OutQuad;
+        [SerializeField] private float _buttonMoveDistance = 100f;
 
         private Sequence _sequence;
+        private Vector2 _rollCardsButtonStartPos;
+        private Vector2 _selectButtonStartPos;
 
         public void Initialize()
         {
+            _rollCardsButtonStartPos = _rollCardsButton.anchoredPosition;
+            
+            _selectButtonStartPos = _selectButton.anchoredPosition;
+            
             foreach (CanvasGroup canvasGroup in _canvasGroups)
             {
                 canvasGroup.alpha = 0f;
@@ -52,11 +61,33 @@ namespace Code.UI.Game.CardSelection
 
             Sequence sequence = DOTween.Sequence().SetUpdate(true);
             _sequence = sequence;
-
+            
             foreach (var canvasGroup in _canvasGroups.Where(canvasGroup => canvasGroup))
             {
                 sequence.Join(canvasGroup
                     .DOFade(visible ? 1f : 0f, _duration)
+                    .SetEase(_ease));
+            }
+            
+            float buttonAnimationDuration = _duration * 0.5f;
+            
+            if (_rollCardsButton != null)
+            {
+                Vector2 targetPos = visible ? _rollCardsButtonStartPos : 
+                    new Vector2(_rollCardsButtonStartPos.x - _buttonMoveDistance, _rollCardsButtonStartPos.y);
+                
+                sequence.Join(_rollCardsButton
+                    .DOAnchorPos(targetPos, buttonAnimationDuration)
+                    .SetEase(_ease));
+            }
+
+            if (_selectButton != null)
+            {
+                Vector2 targetPos = visible ? _selectButtonStartPos : 
+                    new Vector2(_selectButtonStartPos.x + _buttonMoveDistance, _selectButtonStartPos.y);
+                
+                sequence.Join(_selectButton
+                    .DOAnchorPos(targetPos, buttonAnimationDuration)
                     .SetEase(_ease));
             }
 
